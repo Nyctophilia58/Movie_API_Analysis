ecf3654893fa194c38f3c211e0187111e434f00f
Nyctophilia58 <nowshingdr@protonmail.com>
2022-08-27 01:42:49 +0600

    Finished making a new csv file named movies.csv

--- movie_requests.py
+++ movie_requests.py
@@ -8,23 +8,25 @@ def request_data():
     with open('oscar_winners.csv', newline='') as oscar_winners_csv:
         oscar_winners = csv.reader(oscar_winners_csv, delimiter=',')
         next(oscar_winners)
-        rows = oscar_winners
-        for row in rows:
-            movie_id = row[1]
-            url = f'http://www.omdbapi.com/?i={movie_id}&apikey={api_key}'
-            response = requests.get(url)
-            data = response.json()
-            header = ['Movie Title', 'Runtime', 'Genre', 'Award wins', 'Award nominations', 'Box Office']
-            movie_title = data['Title']
-            runtime = data['Runtime']
-            genre = data['Genre']
-            award_wins = int(data['Awards'].split(' ')[1]) + int(data['Awards'].split(' ')[3])
-            award_nominations = int(data['Awards'].split(' ')[6])
-            # box_office = data['BoxOffice']
-            info = [movie_title, runtime, genre, award_wins, award_nominations, box_office]
+        rows = list(oscar_winners)
+        header = ['Movie Title', 'Runtime', 'Genre', 'Award wins', 'Award nominations', 'Box Office']
+        with open('movies.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+            for row in rows:
+                movie_id = row[1]
+                url = f'http://www.omdbapi.com/?i={movie_id}&apikey={api_key}'
+                response = requests.get(url)
+                data = response.json()
+                movie_title = data['Title']
+                runtime = int(data['Runtime'].rstrip(' min'))
+                genre = data['Genre']
+                award_wins = int(data['Awards'].split(' ')[1]) + int(data['Awards'].split(' ')[3])
+                award_nominations = int(data['Awards'].split(' ')[6])
+                box_office = data['BoxOffice'].lstrip('$')
+                box_office = int(box_office.replace(',', ''))
+                info = [movie_title, runtime, genre, award_wins, award_nominations, box_office]
+                writer.writerow(info)
 
-
-
-info = []
 
 request_data()
