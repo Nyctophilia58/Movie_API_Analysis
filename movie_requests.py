7d49711a345e34aa7b14727ec7a67c77bc0ee0f6
Nyctophilia58 <nowshingdr@protonmail.com>
2022-09-01 18:34:14 +0600

    Fixed some of the mistakes

--- movie_requests.py
+++ movie_requests.py
@@ -9,7 +9,7 @@ def request_data():
         oscar_winners = csv.reader(oscar_winners_csv, delimiter=',')
         next(oscar_winners)
         rows = list(oscar_winners)
-        header = ['Movie Title', 'Runtime', 'Genre', 'Award wins', 'Award nominations', 'Box Office']
+        header = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations', 'Box Office']
         with open('movies.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerow(header)
@@ -39,27 +39,27 @@ def add_five_movies():
         writer.writerow(['My Fair Lady', 'tt0058385'])
 
 
-def add_data_of_new_movies():
-    with open('oscar_winners.csv', newline='') as oscar_winners_csv:
-        oscar_winners = csv.reader(oscar_winners_csv, delimiter=',')
-        next(oscar_winners)
-        rows = list(oscar_winners)
-        with open('movies.csv', 'a') as f:
-            writer = csv.writer(f)
-            for row in rows[10:]:
-                movie_id = row[1]
-                url = f'http://www.omdbapi.com/?i={movie_id}&apikey={api_key}'
-                response = requests.get(url)
-                data = response.json()
-                movie_title = data['Title']
-                runtime = int(data['Runtime'].rstrip(' min'))
-                genre = data['Genre']
-                award_wins = int(data['Awards'].split(' ')[1]) + int(data['Awards'].split(' ')[3])
-                award_nominations = int(data['Awards'].split(' ')[6])
-                box_office = data['BoxOffice'].lstrip('$')
-                box_office = int(box_office.replace(',', ''))
-                info = [movie_title, runtime, genre, award_wins, award_nominations, box_office]
-                writer.writerow(info)
+# def add_data_of_new_movies():
+#     with open('oscar_winners.csv', newline='') as oscar_winners_csv:
+#         oscar_winners = csv.reader(oscar_winners_csv, delimiter=',')
+#         next(oscar_winners)
+#         rows = list(oscar_winners)
+#         with open('movies.csv', 'a') as f:
+#             writer = csv.writer(f)
+#             for row in rows[10:]:
+#                 movie_id = row[1]
+#                 url = f'http://www.omdbapi.com/?i={movie_id}&apikey={api_key}'
+#                 response = requests.get(url)
+#                 data = response.json()
+#                 movie_title = data['Title']
+#                 runtime = int(data['Runtime'].rstrip(' min'))
+#                 genre = data['Genre']
+#                 award_wins = int(data['Awards'].split(' ')[1]) + int(data['Awards'].split(' ')[3])
+#                 award_nominations = int(data['Awards'].split(' ')[6])
+#                 box_office = data['BoxOffice'].lstrip('$')
+#                 box_office = int(box_office.replace(',', ''))
+#                 info = [movie_title, runtime, genre, award_wins, award_nominations, box_office]
+#                 writer.writerow(info)
 
 
 def add_additional_columns():
@@ -67,7 +67,7 @@ def add_additional_columns():
         oscar_winners = csv.reader(oscar_winners_csv, delimiter=',')
         next(oscar_winners)
         rows = list(oscar_winners)
-        header = ['Movie Title', 'Runtime', 'Genre', 'Award wins', 'Award nominations',
+        header = ['Movie Title', 'Runtime', 'Genre', 'Award Wins', 'Award Nominations',
                   'Box Office', 'Rated', 'Director', 'Released']
         with open('movies.csv', 'w') as f:
             writer = csv.writer(f)
@@ -86,23 +86,13 @@ def add_additional_columns():
                 box_office = int(box_office.replace(',', ''))
                 rated = data['Rated']
                 director = data['Director']
-                released = clean_date(data['Released'])
+                released = data['Released']
                 info = [movie_title, runtime, genre, award_wins, award_nominations, box_office,
                         rated, director, released]
                 writer.writerow(info)
 
 
-def clean_date(date_str):
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
-              'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    split_date = date_str.split(' ')
-    month = int(months.index(split_date[1]) + 1)
-    day = int(split_date[0].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
-
-
 request_data()
-add_five_movies()
-add_data_of_new_movies()
+# add_five_movies()
+# add_data_of_new_movies()
 add_additional_columns()
